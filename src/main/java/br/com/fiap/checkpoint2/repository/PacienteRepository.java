7e084b3b88c6966e77e7b73f99106c6ae4c8b952
Lucas de Lima <lucasdelimabzr@gmail.com>
2025-05-08 12:47:25 -0300

    feat: repository

--- src/main/java/br/com/fiap/checkpoint2/repository/PacienteRepository.java
+++ src/main/java/br/com/fiap/checkpoint2/repository/PacienteRepository.java
@@ -1,4 +1,9 @@
 package br.com.fiap.checkpoint2.repository;
 
-public class PacienteRepository {
+import br.com.fiap.checkpoint2.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
--- src/main/java/br/com/fiap/checkpoint2/repository/ProfissionalRepository.java
+++ src/main/java/br/com/fiap/checkpoint2/repository/ProfissionalRepository.java
@@ -1,4 +1,9 @@
 package br.com.fiap.checkpoint2.repository;
 
-public class ProfissionalRepository {
+import br.com.fiap.checkpoint2.model.Profissional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfissionalRepository extends JpaRepository<Profissional, Long> {
 }
