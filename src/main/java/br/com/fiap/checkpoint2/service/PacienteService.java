0b90d5763f895924bff27ee78b3e049490442a44
Lucas de Lima <lucasdelimabzr@gmail.com>
2025-05-08 13:08:40 -0300

    feat: service

--- src/main/java/br/com/fiap/checkpoint2/service/PacienteService.java
+++ src/main/java/br/com/fiap/checkpoint2/service/PacienteService.java
@@ -1,4 +1,47 @@
 package br.com.fiap.checkpoint2.service;
 
+import br.com.fiap.checkpoint2.dto.paciente.PacienteRequestCreate;
+import br.com.fiap.checkpoint2.dto.paciente.PacienteRequestUpdate;
+import br.com.fiap.checkpoint2.model.Paciente;
+import br.com.fiap.checkpoint2.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PacienteService {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public Paciente createPaciente(PacienteRequestCreate dto) {
+        return pacienteRepository.save(dto.toModel());
+    }
+
+    public Optional<Paciente> updatePaciente(
+            Long id, PacienteRequestUpdate dto) {
+
+        return pacienteRepository.findById(id)
+                .map(p -> pacienteRepository.save(dto.toModel(p)));
+    }
+
+    public Optional<Paciente> getPacienteById(Long id) {
+        return pacienteRepository.findById(id);
+    }
+
+    public List<Paciente> getAll() {
+        return pacienteRepository.findAll();
+    }
+
+    public boolean deletePaciente(Long id) {
+        if (pacienteRepository.existsById(id)) {
+            pacienteRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
--- src/main/java/br/com/fiap/checkpoint2/service/ProfissionalService.java
+++ src/main/java/br/com/fiap/checkpoint2/service/ProfissionalService.java
@@ -1,4 +1,47 @@
 package br.com.fiap.checkpoint2.service;
 
+import br.com.fiap.checkpoint2.dto.profissional.ProfissionalRequestCreate;
+import br.com.fiap.checkpoint2.dto.profissional.ProfissionalRequestUpdate;
+import br.com.fiap.checkpoint2.model.Profissional;
+import br.com.fiap.checkpoint2.repository.ProfissionalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProfissionalService {
+
+    @Autowired
+    private ProfissionalRepository profissionalRepository;
+
+    public Profissional createProfissional(ProfissionalRequestCreate dto) {
+        return profissionalRepository.save(dto.toModel());
+    }
+
+    public Optional<Profissional> updateProfissional(
+            Long id, ProfissionalRequestUpdate dto) {
+
+        return profissionalRepository.findById(id)
+                .map(p -> profissionalRepository.save(dto.toModel(p)));
+    }
+
+    public Optional<Profissional> getProfissionalById(Long id) {
+        return profissionalRepository.findById(id);
+    }
+
+    public List<Profissional> getAll() {
+        return profissionalRepository.findAll();
+    }
+
+    public boolean deleteProfissional(Long id) {
+        if (profissionalRepository.existsById(id)) {
+            profissionalRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
